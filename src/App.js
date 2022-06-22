c07c27cef75483c2420dc2961e866cedae00ca3d
originlimit <andres-log@protonmail.com>
2022-06-22 14:51:37 -0700

    finished spline design. Working preciew is now fully linked to the page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import React from 'react';
+import Spline from '@splinetool/react-spline';
+
 function App() {
   return (
-    <div className="flex w-screen h-screen items-center justify-center">
-      this is a test
+    <div className="flex w-50% h-50% justify-center">
+      <Spline scene="https://prod.spline.design/d2DsjJHE8A8tVG-z/scene.splinecode" />
     </div>
   );
 }
