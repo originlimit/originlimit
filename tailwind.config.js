786a2d76424fd33ed1124ab4ea3e162fbb469f0a
originlimit <andres-log@protonmail.com>
2022-06-23 19:13:19 -0700

    defined catppuccin colors in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,24 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        base: "#1e1e2e",
+        crust: "#11111b",
+        textBase: "#cdd6f4",
+        subtextBase: "#a6adc8",
+        subtextAlt: "#bac2de",
+        blue: "#89b4fa",
+        red: "#f38ba8",
+        lavender: "#b4befe",
+        yellow: "#f9e2af",
+        peach: "#fab387",
+        green: "#a6e3a1"
+      },
+      margin: {
+        //to be defined
+      }
+    },
   },
   plugins: [],
 }
