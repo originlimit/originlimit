a72aa252903a041061c18610600c46771d660b91
originlimit <andres-log@protonmail.com>
2022-06-29 00:59:06 -0700

    added separator behind nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import SplineFrame from './components/SplineFrame';
 import Navbar from './components/Navbar';
+import Separator from './components/Separator';
 
 function App() {
   return (
     <div className="block justify-center bg-crust">
       <Navbar />
+      <Separator />
       <SplineFrame />
       <p className='text-blue'>This is a text test!</p>
     </div>
---
+++ src/components/Separator.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// this goes under the navbar so the canvas never starts behind the blur
+const Separator = () => {
+  return (
+    <div className='flex p-7'></div>
+  )
+}
+
+export default Separator
