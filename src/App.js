bf04bbfeb0c9468fc9e1b672ccbf1f1005d9bb39
originlimit <andres-log@protonmail.com>
2022-06-29 02:35:04 -0700

    made and added about me component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SplineFrame from './components/SplineFrame';
 import Navbar from './components/Navbar';
 import Separator from './components/Separator';
+import AboutMe from './components/AboutMe';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Separator />
       <SplineFrame />
+      <AboutMe />
     </div>
   );
 }
---
+++ src/components/AboutMe.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AboutMe = () => {
+  return (
+    <div>This is a component that holds info about me</div>
+  )
+}
+
+export default AboutMe
