faeecc22217b32a2e304cc12f3b9fd969968c3e7
originlimit <andres-log@protonmail.com>
2022-06-29 17:00:06 -0700

    about me abstract

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const AboutMe = () => {
   return (
-    <div>This is a component that holds info about me</div>
+    <div className='flex flex-col items-center text-textBase'>
+      <div className='flex max-w-40'>Name and picture</div>
+      <div className='flex'>Description and general interests</div>
+      <div className='flex'>tools i use</div>
+    </div>
   )
 }
 
