7ee342e2870c59e211c72c795e4bd84d224b0d66
originlimit <andres-log@protonmail.com>
2022-07-08 06:34:29 -0700

    style changes

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -2,10 +2,11 @@ import React from 'react'
 
 const AboutMe = () => {
   return (
-    <div className='flex flex-col items-center text-textBase h-screen'>
-      <div className='flex'>
-        This is a paragraph about me.
-      </div>
+    <div className='flex flex-col items-center'>
+    <div className='text-textBase h-screen w-full max-w-xl'>
+      <h2 className='flex text-xl font-bold text-left underline decoration-blue underline-offset-8 my-4'>Bio</h2>
+      <p className='text-textBase'>I am curently a computer science student. I like to expand my skills and learn new things. I also like to design UIs and work with Linux machines to make them look and function better. My interests range from software to philosophy — I am interested in the privacy and security of the applications we use in our day-to-day.</p>
+    </div>
     </div>
   )
 }
