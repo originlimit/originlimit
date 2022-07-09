61f22d4914c38e666881028d570ef55feb37a1d1
originlimit <andres-log@protonmail.com>
2022-07-09 10:14:44 -0700

    wrapped header contents in div and centered it. Modified text size and logo text

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,15 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='fixed flex w-screen z-99 top-0 p-4 content-between justify-between bg-crustOpaque backdrop-blur-md'>
-      <div className='text-lavender'>A Portfolio!</div>
-      <div>
-        <a href='/#' className='text-red'>test</a>
-        <a href='/#' className='text-red'>test</a>
-        <a href='/#' className='text-red'>test</a>
-        <a href='/#' className='text-red'>test</a>
+    <div className='fixed flex justify-center w-screen z-99 top-0 bg-crustOpaque backdrop-blur-md p-4'>
+      <div className='flex w-full max-w-3xl justify-between text-lg'>
+        <div className='font-bold text-lavender'>Originlimit</div>
+        <div>
+          <a href='/#' className='text-red'>test</a>
+          <a href='/#' className='text-red'>test</a>
+          <a href='/#' className='text-red'>test</a>
+          <a href='/#' className='text-red'>test</a>
+        </div>
       </div>
     </div>
   )
