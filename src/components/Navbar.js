9d2951ad3a67dafc9c028490aec89ed90b9a8b51
originlimit <andres-log@protonmail.com>
2022-07-10 10:39:13 -0700

    chaged nav titles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,10 +6,9 @@ const Navbar = () => {
       <div className='flex w-full max-w-3xl justify-between text-lg'>
         <div className='font-bold text-lavender'>Originlimit</div>
         <div>
-          <a href='/#' className='text-red'>test</a>
-          <a href='/#' className='text-red'>test</a>
-          <a href='/#' className='text-red'>test</a>
-          <a href='/#' className='text-red'>test</a>
+          <a href='/#' className='text-red px-2'>Works</a>
+          <a href='/#' className='text-red px-2'>Source</a>
+          <a href='/#' className='text-red px-2'>Dark Mode</a>
         </div>
       </div>
     </div>
