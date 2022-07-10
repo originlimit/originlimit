6b7ee2fc3791a2c759c6db78f9a4740ba329b65d
originlimit <andres-log@protonmail.com>
2022-07-10 10:55:02 -0700

    styled button. Arrow icon needed

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -8,7 +8,7 @@ const AboutMe = () => {
       <p className='text-textBase'>I am curently a computer science student. I like to expand my skills and learn new things. I also like to design UIs and work with Linux machines to make them look and function better. My interests range from software to philosophy — I am interested in the privacy and security of the applications we use in our day-to-day.</p>
       <p className='text-textBase'>Most of my free time involves studying various subjects related to computer science or its subfields. I also work on several unreleased projects (mobile apps, Linux scripts, etc.) and contribute in privacy oriented communities in form of moderation and providing information.</p>
       <div className='flex justify-center'>
-        <button className='bg-blue'>Hello, world!</button>
+        <button className='bg-red text-crust py-2 px-4 m-8 rounded-lg font-bold'>My Portfolio</button>
       </div>
     </div>
     </div>
