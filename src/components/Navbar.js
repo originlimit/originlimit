406e79d07ddd2aaa9b2b7d8b3a00188f0669bf2a
originlimit <andres-log@protonmail.com>
2022-07-10 11:55:56 -0700

    styled and centered navlist. added icons and wrapped nav and dark mode button in div

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,18 @@
 import React from 'react'
+import { BiSun } from 'react-icons/bi'
 
 const Navbar = () => {
   return (
     <div className='fixed flex justify-center w-screen z-99 top-0 bg-crustOpaque backdrop-blur-md p-4'>
       <div className='flex w-full max-w-3xl justify-between text-lg'>
-        <div className='font-bold text-lavender'>Originlimit</div>
-        <div>
-          <a href='/#' className='text-red px-2'>Works</a>
-          <a href='/#' className='text-red px-2'>Source</a>
-          <button className='text-red px-2'>Dark Mode</button>
+        <a className='font-bold text-lavender'>Originlimit •</a>
+        <div className='flex'>
+          <nav>
+            <a href='/#' className='text-red m-2'>Works</a>
+            <a href='/#' className='text-red m-2'>Contact</a>
+            <a href='/#' className='text-red m-2'>Source</a>
+          </nav>
+          <button className='text-yellow mx-2 rounded-lg text-bold text-3xl'><BiSun /></button>
         </div>
       </div>
     </div>
