d3bdc4bb5340ff5061278bfcc8375669497a477d
originlimit <andres-log@protonmail.com>
2022-07-13 14:02:52 -0700

    component now checks id and displays the title if it is the first entry

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Timeline = (props) => {
   return (
     <div className='flex flex-col justify-center items-center text-blue'>
-      <h2>Timeline</h2>
+      <h2>{ props.id === 1 ? "Timeline" : null }</h2>
       <div>
         <h4>{ props.date }</h4>
         <p>{ props.desc }</p>
