00035ced7885e025c0d690b59b2bdca0638129bc
originlimit <andres-log@protonmail.com>
2022-07-13 15:01:39 -0700

    minor styling changes

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -7,9 +7,9 @@ const Timeline = (props) => {
     <div className='flex justify-center'>
       <div className='flex flex-col text-textBase w-full max-w-xl'>
         { props.id === 1 ? <h2 className='flex text-lg font-bold text-left underline decoration-2 decoration-blue underline-offset-8 my-4'>Timeline</h2> : null }
-        <div className='flex'>
-          <h4 className='max-w-{20%} mr-4 font-bold'>{ props.date }</h4>
-          <p className='max-w-full'>{ props.desc }</p>
+        <div className='flex mb-2'>
+          <h4 className='min-w-{20%} mr-2 font-bold'>({ props.date })</h4>
+          <p className=''>{ props.desc }</p>
         </div>
       </div>
     </div>
