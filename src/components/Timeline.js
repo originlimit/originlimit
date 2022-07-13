7cfa9eda414c68bce2bcfa502e13d35865f6a5f8
originlimit <andres-log@protonmail.com>
2022-07-13 14:33:03 -0700

    changed ternary to make title not exist unless on top, formatting, styling changes, and better structure

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,15 +1,16 @@
 import React from 'react'
 
-
-//component now checks the id prop and only displays the h1 if it is of id 1
+//component now checks the id prop and only displays the title if it is of id 1
 
 const Timeline = (props) => {
   return (
-    <div className='flex flex-col justify-center items-center text-blue'>
-      <h2>{ props.id === 1 ? "Timeline" : null }</h2>
-      <div>
-        <h4>{ props.date }</h4>
-        <p>{ props.desc }</p>
+    <div className='flex justify-center'>
+      <div className='flex flex-col text-textBase w-full max-w-xl'>
+        { props.id === 1 ? <h2 className='flex text-lg font-bold text-left underline decoration-2 decoration-blue underline-offset-8 my-4'>Timeline</h2> : null }
+        <div className='flex'>
+          <h4 className='max-w-{20%} mr-4 font-bold'>{ props.date }</h4>
+          <p className='max-w-full'>{ props.desc }</p>
+        </div>
       </div>
     </div>
   )
