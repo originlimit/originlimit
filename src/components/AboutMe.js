ac8dfb9d2bf284c4ac17e49229506976ac5635e5
originlimit <andres-log@protonmail.com>
2022-07-13 15:30:04 -0700

    minor styling changes and the addition of tools i use

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -6,10 +6,11 @@ const AboutMe = () => {
     <div className='flex flex-col items-center'>
       <div className='text-textBase w-full max-w-xl'>
         <h2 className='flex text-lg font-bold text-left underline decoration-2 decoration-blue underline-offset-8 my-4'>Bio</h2>
-        <p className='text-textBase'>I am curently a computer science student. I like to expand my skills and learn new things. I also like to design UIs and work with Linux machines to make them look and function better. My interests range from software to philosophy — I am interested in the privacy and security of the applications we use in our day-to-day.</p>
-        <p className='text-textBase'>Most of my free time involves studying various subjects related to computer science or its subfields. I also work on several unreleased projects (mobile apps, Linux scripts, etc.) and contribute in privacy oriented communities in form of moderation and providing information.</p>
+        <p>I am curently a computer science student. I like to expand my skills and learn new things. I also like to design UIs and work with Linux machines to make them look and function better. My interests range from software to philosophy — I am interested in the privacy and security of the applications we use in our day-to-day.</p>
+        <p>Most of my free time involves studying various subjects related to computer science or its subfields. I also work on several unreleased projects (mobile apps, Linux scripts, etc.) and contribute in privacy oriented communities in form of moderation and providing information.</p>
+        <p className='my-4 text-blue'><strong>Tools I use</strong> — ReactJS, TailwindCSS, Figma, HTML, CSS, JavaScript (ES6), Linux, GIT, Node, Shopify, Wordpress.</p>
         <div className='flex justify-center'>
-          <button className='flex bg-red text-crust py-2 px-4 m-8 rounded-lg font-bold hover:bg-blue'>My Portfolio<BiRightArrowAlt className='text-2xl pl-1'/></button>
+          <button className='flex bg-red text-crust py-2 px-4 m-4 rounded-lg font-bold hover:bg-blue'>My Portfolio<BiRightArrowAlt className='text-2xl pl-1'/></button>
         </div>
       </div>
     </div>
