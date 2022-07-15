b8f39d63271046e1b67b0e82c993dba46804217e
originlimit <andres-log@protonmail.com>
2022-07-15 11:00:30 -0700

    replaced hardcode title with title component

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import SectionTitle from './SectionTitle'
 
 //component now checks the id prop and only displays the title if it is of id 1
 
@@ -6,7 +7,7 @@ const Timeline = (props) => {
   return (
     <div className='flex justify-center'>
       <div className='flex flex-col text-textBase w-full max-w-xl'>
-        { props.id === 1 ? <h2 className='flex text-lg font-bold text-left underline decoration-2 decoration-blue underline-offset-8 my-4'>Timeline</h2> : null }
+        { props.id === 1 ? <SectionTitle title='Timeline' /> : null }
         <div className='flex mb-2'>
           <h4 className='min-w-{20%} mr-2 font-bold'>({ props.date })</h4>
           <p className=''>{ props.desc }</p>
