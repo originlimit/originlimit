22d8dccf97a66ad76e808b519b701ed7014e7986
originlimit <andres-log@protonmail.com>
2022-07-19 15:04:29 -0700

    made pages

---
+++ src/pages/bioIndex.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const bioIndex = () => {
+  return (
+    <div>bioIndex</div>
+  )
+}
+
+export default bioIndex
---
+++ src/pages/worksIndex.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const worksIndex = () => {
+  return (
+    <div>worksIndex</div>
+  )
+}
+
+export default worksIndex
