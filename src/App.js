e5f35206c315efd1ca57bd5810ab88b08d02998a
originlimit <andres-log@protonmail.com>
2022-07-31 13:01:27 -0700

    removed to do comment and removed unused imports

--- src/App.js
+++ src/App.js
@@ -2,17 +2,10 @@ import React from 'react';
 import SplineFrame from './components/SplineFrame';
 import Navbar from './components/Navbar';
 import Separator from './components/Separator';
-import AboutMe from './components/AboutMe';
-import Profile from './components/Profile';
-import Timeline from './components/Timeline';
-import timelineData from './data/timeline.json';
 import Footer from './components/Footer';
-import Contact from './components/Contact';
-import WorksIndex from './pages/WorksIndex';
 import BioIndex from './pages/BioIndex';
 
 // function on timelinedata maps properties to timeline component from timeline.json
-// move the components that arent always on screen to page component
 
 function App() {
   return (
