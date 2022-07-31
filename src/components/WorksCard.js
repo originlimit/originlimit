c4b345a231357a94bab55931b61c96be4a317299
originlimit <andres-log@protonmail.com>
2022-07-31 15:19:48 -0700

    styled cards

--- src/components/WorksCard.js
+++ src/components/WorksCard.js
@@ -3,10 +3,12 @@ import React from 'react'
 const WorksCard = (props) => {
   return (
     <div className='w-1/2'>
-      <div className='min-w-max bg-red p-4'>
+      <div className='flex flex-col flex-wrap bg-base p-4 m-2 rounded-md'>
         <img className='h-1/2' src={ props.img } alt={ props.alt }></img>
-        <h4 className='text-lg'>{ props.title }</h4>
-        <p className='flex'>{ props.desc }</p>
+        <div className='flex flex-wrap h-1/2'>
+          <h4 className='text-lg text-blue'>{ props.title }</h4>
+          <p>{ props.desc }</p>
+        </div>
       </div>
     </div>
   )
