2bf3ee9a9dd5ed81d2b3028e1b1e18f84958d683
originlimit <andres-log@protonmail.com>
2022-07-31 13:21:51 -0700

    added documentation

--- src/pages/WorksIndex.js
+++ src/pages/WorksIndex.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Works from '../components/Works'
 
+// each of the subcomponents will use a map function that goes through JSON data arrays to pass props
+// to their respective components
+
 const WorksIndex = () => {
   return (
     <>
