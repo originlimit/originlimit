df08290041e9c7165f9af29cc789cb6b35308c8a
originlimit <andres-log@protonmail.com>
2022-07-31 13:02:56 -0700

    cleaned up component

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import Separator from './components/Separator';
 import Footer from './components/Footer';
 import BioIndex from './pages/BioIndex';
 
-// function on timelinedata maps properties to timeline component from timeline.json
-
 function App() {
   return (
     <div className="bg-crust m-0 p-0">
