fb082aca4b5ef595add02e3d33b493d253b8bd96
originlimit <andres-log@protonmail.com>
2022-07-31 14:36:41 -0700

    removed works

--- src/pages/BioIndex.js
+++ src/pages/BioIndex.js
@@ -3,7 +3,6 @@ import AboutMe from '../components/AboutMe';
 import Profile from '../components/Profile';
 import Timeline from '../components/Timeline';
 import Contact from '../components/Contact';
-import WorksIndex from '../pages/WorksIndex';
 import timelineData from '../data/timeline.json'
 
 // function on timelinedata maps properties to timeline component from timeline.json
@@ -14,7 +13,6 @@ const BioIndex = () => {
       <Profile />
       <AboutMe />
       {timelineData.map((timeline) => <Timeline key={timeline.id} id={timeline.id} date={timeline.date} desc={timeline.desc} />)}
-      <WorksIndex />
       <Contact />
     </div>
   )
