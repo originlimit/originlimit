b89987578b86ac393a83c3335439979c6edb7a08
originlimit <andres-log@protonmail.com>
2022-07-31 15:20:47 -0700

    centered cards and made them space properly

--- src/components/Works.js
+++ src/components/Works.js
@@ -4,7 +4,7 @@ import worksData from '../data/works.json'
 
 const Works = () => {
   return (
-    <div className='flex flex-wrap max-w-xl'>
+    <div className='flex flex-wrap max-w-xl justify-center items-center m-auto'>
       {worksData.map((works) => <WorksCard key={works.id} id={works.id} img={works.img} alt={works.alt} title={works.title} desc={works.desc} />)}
     </div>
   )
