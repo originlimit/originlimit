e6012ed12c550305836f282c4b3a06c684d543c7
originlimit <andres-log@protonmail.com>
2022-08-04 15:25:33 -0700

    switched padding to child container

--- src/components/WorksCard.js
+++ src/components/WorksCard.js
@@ -3,9 +3,9 @@ import React from 'react'
 const WorksCard = (props) => {
   return (
     <div className='w-1/2'>
-      <div className='flex flex-col flex-wrap bg-base p-4 m-2 rounded-md'>
+      <div className='flex flex-col flex-wrap bg-base m-2 rounded-md'>
         <img className='h-1/2' src={ props.img } alt={ props.alt }></img>
-        <div className='flex flex-wrap h-1/2'>
+        <div className='flex flex-wrap h-1/2 p-4'>
           <h4 className='text-lg text-blue font-bold'>{ props.title }</h4>
           <p>{ props.desc }</p>
         </div>
