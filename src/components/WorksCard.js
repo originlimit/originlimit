9cd5b0669dd553982222051d9c3b5cd052c8cbc2
originlimit <andres-log@protonmail.com>
2022-08-04 15:27:32 -0700

    rounded top of image element

--- src/components/WorksCard.js
+++ src/components/WorksCard.js
@@ -4,7 +4,7 @@ const WorksCard = (props) => {
   return (
     <div className='w-1/2'>
       <div className='flex flex-col flex-wrap bg-base m-2 rounded-md'>
-        <img className='h-1/2' src={ props.img } alt={ props.alt }></img>
+        <img className='h-1/2 rounded-t-md' src={ props.img } alt={ props.alt }></img>
         <div className='flex flex-wrap h-1/2 p-4'>
           <h4 className='text-lg text-blue font-bold'>{ props.title }</h4>
           <p>{ props.desc }</p>
